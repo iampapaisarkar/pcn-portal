69d64f37541794941586c43b872ad658bcf61142
Papai Sarkar <iampapaisarkar@gmail.com>
2021-07-17 20:03:26 +0530

    login override done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,20 +8,33 @@
                 <div class="col-md-6">
                     <div class="p-4">
                         <div class="auth-logo text-center mb-4"><img src="{{asset('admin/dist-assets/images/logo.png')}}" alt=""></div>
-                        <h1 class="mb-3 text-18">Sign In</h1>
-                        <form action="dashboard.php">
+                        <h1 class="mb-3 text-18">{{ __('Login') }}</h1>
+                        <form method="POST" action="{{ route('login') }}">
+                        @csrf
                             <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input class="form-control form-control" id="email" type="email">
+                                <label for="email">{{ __('E-Mail Address') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <label for="password">Password</label>
-                                <input class="form-control form-control" id="password" type="password">
+                                <label for="password">{{ __('Password') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <button class="btn btn btn-primary btn-block mt-2">Sign In</button>
+                            <button type="submit" class="btn btn btn-primary btn-block mt-2">{{ __('Login') }}</button>
                         </form>
+                        @if (Route::has('password.request'))
                         <div class="mt-3 text-center"><a class="text-muted" href="{{ route('password.request') }}">
-                                <u>Forgot Password?</u></a></div>
+                                <u>{{ __('Forgot Your Password?') }}</u></a></div>
+                        @endif
                     </div>
                 </div>
                 <div class="col-md-6 text-center" style="background-size: cover;background-image: url({{asset('admin/dist-assets/images/photo-long-3.jpg')}})">
