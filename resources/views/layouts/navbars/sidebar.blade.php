bf372c824a74953f82d604b464a2c9cf5b856f67
Papai Sarkar <iampapaisarkar@gmail.com>
2021-07-17 23:25:57 +0530

    added gates for user authorization

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -21,54 +21,210 @@
         
         <div class="submenu-area" data-parent="admin">
             <header>
+                @if(Auth::user()->hasRole('sadmin'))
                 <h6>HQ Abuja</h6>
                 <p>Super Admin</p>
+                @endif
+                @if(Auth::user()->hasRole('state_office'))
+                <h6>PCN State Office</h6>
+                <p>State Office</p>
+                @endif
+                @if(Auth::user()->hasRole('pharmacy_practice'))
+                <h6>PCN HQ Abuja</h6>
+                <p>Pharmacy Practice</p>
+                @endif
+                @if(Auth::user()->hasRole('registration_licencing'))
+                <h6>PCN HQ Abuja</h6>
+                <p>Registration and Licencing</p>
+                @endif
+                @if(Auth::user()->hasRole('vendor'))
+                <h6>PPMV</h6>
+                <p>Tiered PPMV</p>
+                @endif
                 
             </header>
             <ul class="childNav">
                 <li class="nav-item">
-                    <a href="admin-dashboard.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Bar-Chart"></i>
                         <span class="item-name">Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="admin-profile.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Bar-Chart"></i>
                         <span class="item-name">Profile</span>
                     </a>
                 </li>
                 
+                <!-- Super admin routes start  -->
+                @can('isAdmin')
                 <li class="nav-item">
-                    <a href="admin-users.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Receipt-4"></i>
                         <span class="item-name">Admin Users</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="admin-schools.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Receipt-4"></i>
                         <span class="item-name">Training Schools</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="admin-batch.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Receipt-4"></i>
                         <span class="item-name">MEPTP Batches</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="admin-services.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Receipt-4"></i>
                         <span class="item-name">Service Fees</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="admin-invoices.php">
+                    <a href="{{route('dashboard')}}">
                         <i class="nav-icon i-Receipt-4"></i>
                         <span class="item-name">Payments</span>
                     </a>
                 </li>
+                @endcan
+                <!-- Super admin routes end  -->
+
+                <!-- State office routes start  -->
+                @can('isSOffice')
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Doc. Review Pending </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Doc. Review Approved </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Training Approved </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration - Doc. Review</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration - Inspection</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration - Report</span>
+                    </a>
+                </li>
+                @endcan
+                <!-- State office routes end  -->
+
+                <!-- Pharmacy practice routes start  -->
+                @can('isPPractice')
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Pending PP Approval</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Training PP Approved</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP - Training Results</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Status</span>
+                    </a>
+                </li>
+                @endcan
+                <!-- Pharmacy practice routes end  -->
+
+                <!-- Registration licencing routes start  -->
+                @can('isRLicencing')
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration - Licence Pending</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">PPMV Registration - Licence Issued</span>
+                    </a>
+                </li>
+                @endcan
+                <!-- Registration licencing routes end  -->
+
+                <!-- Vendor routes start  -->
+                @can('isVendor')
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP Application</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP Application Status</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">MEPTP Result</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">Registration</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">Renewal</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('dashboard')}}">
+                        <i class="nav-icon i-Receipt-4"></i>
+                        <span class="item-name">Invoices</span>
+                    </a>
+                </li>
+                @endcan
+                <!-- Vendor routes end  -->
             </ul>
         </div>
     </div>
