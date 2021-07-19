d54cd9d2ec5ccaa0710350d93df2d3d3e4b9db99
Papai Sarkar <iampapaisarkar@gmail.com>
2021-07-20 00:17:10 +0530

    email logo path fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="<?php echo $_SERVER["DOCUMENT_ROOT"]; ?>'/admin/dist-assets/images/logo.png'" class="logo" alt="PCN Logo">
+<img src="{{env('APP_URL')}} . '/admin/dist-assets/images/logo.png'" class="logo" alt="PCN Logo">
 @endif
 </a>
 </td>
