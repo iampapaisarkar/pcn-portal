128557a28f1ec0e304ed15970f32df1a76d733f7
Papai Sarkar <iampapaisarkar@gmail.com>
2021-07-27 21:56:26 +0530

    working meptp status

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://iampapaisarkar.xyz/pcn-portal/public/admin/dist-assets/images/logo.png" class="logo" alt="Laravel Logo">
+<img src="https://iampapaisarkar.xyz/pcn-portal/public/admin/dist-assets/images/logo.png" class="logo" alt="PCN Logo">
 @else
 <img src="https://iampapaisarkar.xyz/pcn-portal/public/admin/dist-assets/images/logo.png" class="logo" alt="PCN Logo">
 @endif
