016a47b2d6b3274fed0a3d916c89da6e54b207a6
Papai Sarkar <iampapaisarkar@gmail.com>
2021-08-12 16:12:26 +0530

    email error fix

--- resources/views/mail/approve-tier-email.blade.php
+++ resources/views/mail/approve-tier-email.blade.php
@@ -2,7 +2,7 @@
 # Approve MEPTP Traning Application {{env('APP_NAME')}}
 
 <h2>MEPTP Traning Application approved & selected for Tier</h2>
-<h2>Vendor Name: {{$data['user']['firstname']}} {{$data['user']['lastname']}}</h2>
+<h2>Vendor Name: {{$data['vendor']['firstname']}} {{$data['vendor']['lastname']}}</h2>
 <h2>Batch: {{$data['application']['batch']['batch_no']}}/{{$data['application']['batch']['year']}}</h2>
 <h2>Tier: {{$data['application']['tier']['name']}}</h2>
 
