e10a82b6d77a9e72336a886f267e1a49d9e05b67
Papai Sarkar <iampapaisarkar@gmail.com>
2021-08-17 10:13:32 +0530

    meptp application approved tier email done

--- app/Mail/ApproveTierEmail.php
+++ app/Mail/ApproveTierEmail.php
@@ -28,6 +28,6 @@ class ApproveTierEmail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('mail.approve-tier-email',['data'=>$this->newData])->subject(env('APP_NAME') . ' - Approve MEPTP Registration Application');
+        return $this->markdown('mail.approve-tier-email',['data'=>$this->newData])->subject('MEPTP Application Approval');
     }
 }
--- resources/views/mail/approve-tier-email.blade.php
+++ resources/views/mail/approve-tier-email.blade.php
@@ -1,10 +1,17 @@
 @component('mail::message')
 # Approve MEPTP Traning Application {{env('APP_NAME')}}
 
-<h2>MEPTP Traning Application approved & selected for Tier</h2>
-<h2>Vendor Name: {{$data['vendor']['firstname']}} {{$data['vendor']['lastname']}}</h2>
-<h2>Batch: {{$data['application']['batch']['batch_no']}}/{{$data['application']['batch']['year']}}</h2>
-<h2>Tier: {{$data['application']['tier']['name']}}</h2>
+<h2>Hello {{$data['vendor']['firstname']}} {{$data['vendor']['lastname']}}, <br>
+CONGRATULATIONS.
+</h2>
+
+<h2>There is to inform you that your application for the MEPTP at the {{$data['application']['school']['name']}} has been approvedVendor Name: {{$data['vendor']['firstname']}} {{$data['vendor']['lastname']}}</h2>
+
+<h2>Vendor Name: Vendor NamBatch: {{$data['application']['batch']['batch_no']}}</h2>
+<h2>Batch: batch NumbeTier: {{$data['application']['tier']['name']}}</h2>
+<h2>Tier: Tier TypFurther information will be communicated to you accordingly.</h2>
+<h2>Further information will be communicated to you accordingThank you.</h2>
+<h2>Thank you.</h2>
 
 {{ config('app.name') }}
 @endcomponent
