c4b145cab3800e7694929d7f2750bd337cc321df
Papai Sarkar <iampapaisarkar@gmail.com>
2021-08-17 16:23:41 +0530

    meptp payment success issue fix

--- resources/views/mail/payment-success.blade.php
+++ resources/views/mail/payment-success.blade.php
@@ -3,7 +3,7 @@
 @if($data['type'] == 'meptp_training')
 # MEPTP Payment Notification - {{env('APP_NAME')}}
 
-<h2>Hello {{Auth:user()->firstname}} {{Auth:user()->lastname}}, <br>
+<h2>Hello {{Auth::user()->firstname}} {{Auth::user()->lastname}}, <br>
 This is to acknowledge your application and payment of the sum of N{{$data['amount']}} being made for PCN MANDATORY ENTRY POINT TRAINING PROGRAMME (MEPTP) FOR PATENT AND PROPRIETARY MEDICINE VENDORS (PPMVS).
 </h2>
 <h2>Kindly log in to the portal to view status and application progress.</h2>
@@ -14,7 +14,7 @@ This is to acknowledge your application and payment of the sum of N{{$data['amou
 @if($data['type'] == 'ppmv_registration')
 # Licence Registration Payment - {{env('APP_NAME')}}
 
-<h2>Hello {{Auth:user()->firstname}} {{Auth:user()->lastname}}, <br>
+<h2>Hello {{Auth::user()->firstname}} {{Auth::user()->lastname}}, <br>
 This is to acknowledge your application and payment of the sum of N{{$data['amount']}} being made for PCN LICENCE REGISTRATION FOR PATENT AND PROPRIETARY MEDICINE VENDORS (PPMVS) for the year {{$data['year']}}.
 </h2>
 
@@ -28,7 +28,7 @@ This is to acknowledge your application and payment of the sum of N{{$data['amou
 @if($data['type'] == 'ppmv_renewal')
 # Licence Registration Payment - {{env('APP_NAME')}}
 
-<h2>Hello {{Auth:user()->firstname}} {{Auth:user()->lastname}}, <br>
+<h2>Hello {{Auth::user()->firstname}} {{Auth::user()->lastname}}, <br>
 This is to acknowledge your application and payment of the sum of N{{$data['amount']}} being made for PCN LICENCE REGISTRATION FOR PATENT AND PROPRIETARY MEDICINE VENDORS (PPMVS) for the year {{$data['year']}}.
 </h2>
 
