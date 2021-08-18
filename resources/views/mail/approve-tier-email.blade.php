423a2c9a38a69a5d99f6d2290c83851bb49e0d2e
Papai Sarkar <iampapaisarkar@gmail.com>
2021-08-18 11:27:04 +0530

    approve tier email template design and information change

--- resources/views/mail/approve-tier-email.blade.php
+++ resources/views/mail/approve-tier-email.blade.php
@@ -7,10 +7,8 @@ CONGRATULATIONS.
 
 <div>There is to inform you that your application for the MEPTP at the {{$data['application']['school']['name']}} has been approved</div>
 <div>Vendor Name: {{$data['vendor']['firstname']}} {{$data['vendor']['lastname']}}</div>
-
-<div>Vendor Name: Vendor NamBatch: {{$data['application']['batch']['batch_no']}}</div>
-<div>Batch: batch NumbeTier: {{$data['application']['tier']['name']}}</div>
-<div>Tier: Tier TypFurther information will be communicated to you accordingly.</div>
+<div>Batch: {{$data['application']['batch']['batch_no']}}/{{$data['application']['batch']['year']}}</div>
+<div>Tier: {{$data['application']['tier']['name']}}</div>
 <div>Further information will be communicated to you according.</div>
 <div>Thank you.</div>
 
